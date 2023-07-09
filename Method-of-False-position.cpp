7748915f1cc45be917a753711fd55d95d4223c30
AbdullahRifat <110035643+AbdullahRifat@users.noreply.github.com>
2023-07-09 22:26:00 +0600

    Create Method-of-False-position.cpp

---
+++ Method-of-False-position.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+#define f(x)  x*x*x-2*x-5
+
+using namespace std;
+
+int main() {
+    float a, b, c, fa, fb, fc, e;
+    int maxitr, i;
+
+    cout << "Enter two initial starting roots: ";
+    cin >> a >> b;
+
+    cout << "Enter maximum iteration: ";
+    cin >> maxitr;
+
+    cout << "Enter the tolerance value: ";
+    cin >> e;
+
+    fa = f(a);
+    fb = f(b);
+   
+        for (i = 1; i <= maxitr; i++) {
+            c = (a * fb - b * fa) / (fb - fa);
+            fc = f(c);
+            if (fabs(fc) < e) {
+                cout << "Root of the given equation = " << c << endl;
+                maxitr = i;
+                break;
+            } else if (fc * fa < 0) {
+                b = c;
+                fb = fc;
+            } else {
+                a = c;
+                fa = fc;
+            }
+        }
+    
+    return 0;
+}
+
